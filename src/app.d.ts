7a1cb43fa1ea856865387712610218449011d82c
César M. Cristóbal <cmartin@bitban.com>
2024-09-23 13:39:16 +0200

    add prisma orm

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,16 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				DB: D1Database;
+			};
+			context: {
+				/* eslint-disable  @typescript-eslint/no-explicit-any */
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 	}
 }
 
