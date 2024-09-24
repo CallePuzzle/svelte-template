e28fb7d0f2036517274ef83e589754367c91a9ff
César M. Cristóbal <cesar@callepuzzle.com>
2024-09-24 22:29:42 +0200

    wip notifications

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -2,7 +2,8 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT,
-    "picture" TEXT
+    "picture" TEXT,
+    "subscription" TEXT
 );
 
 -- CreateTable
@@ -12,3 +13,26 @@ CREATE TABLE "Session" (
     "expires_at" DATETIME NOT NULL,
     CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "_NotificationToUser" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_NotificationToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Notification" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_NotificationToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_NotificationToUser_AB_unique" ON "_NotificationToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_NotificationToUser_B_index" ON "_NotificationToUser"("B");
