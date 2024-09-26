22f87ee528bdc8102d57f966ab99ea347eaf1ff6
César M. Cristóbal <cesar@callepuzzle.com>
2024-09-26 16:33:26 +0200

    wip subscription

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
 	import Nav from '$lib/components/layout/Nav.svelte';
 	import toast, { Toaster } from 'svelte-french-toast';
+	import { SubscribeUser } from '$lib/notification/subscribe-user';
+	import { Routes } from '$lib/routes';
 
 	let { children, data } = $props();
 
-	$effect(async () => {
+	onMount(async () => {
 		const status = await Notification.requestPermission();
 		if ('serviceWorker' in navigator && data.userIsLogged) {
 			const reg = await navigator.serviceWorker.ready;
-			await SubscribeUser(data.userIsLogged, data.user.id, reg, data.JWKpublicKey);
+			await SubscribeUser(data.userIsLogged, data.user?.id as string, reg, data.JWKpublicKey);
 		}
 
-		if (data.notifications && data.path !== Routes.notification_my.url) {
-			if (data.notificationsCount == 1) {
-				toast('Tienes una notificación sin leer', {
-					icon: '🔔'
-				});
-			} else if (data.notificationsCount > 1) {
-				toast(`Tienes ${data.notificationsCount} notificaciones sin leer`, {
-					icon: '🔔'
-				});
-			}
-		}
+		//if (data.notifications && data.path !== Routes.notification_my.url) {
+		//	if (data.notificationsCount == 1) {
+		//		toast('Tienes una notificación sin leer', {
+		//			icon: '🔔'
+		//		});
+		//	} else if (data.notificationsCount > 1) {
+		//		toast(`Tienes ${data.notificationsCount} notificaciones sin leer`, {
+		//			icon: '🔔'
+		//		});
+		//	}
+		//}
 	});
 </script>
 
